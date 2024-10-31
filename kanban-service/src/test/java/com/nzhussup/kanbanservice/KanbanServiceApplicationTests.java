a0bbc8d93d9613c4bb0c6d615d755308976df1f2
nzhussup <nurzhanat2002@gmail.com>
2024-10-31 16:21:05 +0100

    Delete the context loading test case

--- kanban-service/src/test/java/com/nzhussup/kanbanservice/KanbanServiceApplicationTests.java
+++ kanban-service/src/test/java/com/nzhussup/kanbanservice/KanbanServiceApplicationTests.java
@@ -1,12 +1,13 @@
 package com.nzhussup.kanbanservice;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 class KanbanServiceApplicationTests {
 
-    @Test
     void contextLoads() {
     }
 
