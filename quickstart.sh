ebdb7f6b612d2fe22b03437c1c91ae6bc3cd5712
nzhussup <nurzhanat2002@gmail.com>
2024-10-31 16:31:24 +0100

    Add quickstart shell script

---
+++ quickstart.sh
@@ -0,0 +1,11 @@
+echo "starting the minicube"
+minikube start
+
+cd k8s/
+
+echo "starting the pods"
+kubectl apply -f configmap.yml
+kubectl apply -f secrets.yml
+kubectl apply -f redis-deployment.yml
+kubectl apply -f db-deployment.yml
+kubectl apply -f kanban-service-deployment.yml
