3652eb6db858eff4252fc5d77976ae2800220153
nzhussup <nurzhanat2002@gmail.com>
2024-11-02 10:40:23 +0100

    Add quickstart for AWS instances

---
+++ aws-quickstart.sh
@@ -0,0 +1,19 @@
+echo "Updating and upgrading apt"
+
+sudo apt update && sudo apt upgrade -y
+
+echo "Installing K3s"
+
+curl -sfL https://get.k3s.io | sh -
+alias kubectl="k3s kubectl"
+sudo chmod 644 /etc/rancher/k3s/k3s.yaml
+
+echo "Starting pods"
+
+cd k8s/
+
+kubectl apply -f secrets.yml
+kubectl apply -f configmap.yml
+kubectl apply -f db-deployment.yml
+kubectl apply -f redis-deployment.yml
+kubectl apply -f kanban-service-deployment.yml
